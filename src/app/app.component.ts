7542b494cc6ae7ace63640d586c6a0786280637f
Antonio DroCode <antuansabe@gmail.com>
2021-12-08 21:12:09 -0600

    finish output component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  imgParent = 'https://www.w3schools.com/howto/img_avatar.png';
+  imgParent = '';
+
+  onLoaded(img: string) {
+    console.log('log padre', img);
+  }
 }
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ImgComponent implements OnInit {
 
   @Input() img: string = '';
+  @Output() loaded = new EventEmitter<string>();
   imageDefault = './assets/default.png';
 
   constructor() { }
@@ -17,6 +18,11 @@ export class ImgComponent implements OnInit {
 
   imgError() {
   this.img = this.imageDefault;
+  }
+
+  imgLoaded() {
+    console.log('log hijo');
+    this.loaded.emit(this.img);
   }
 
 }
