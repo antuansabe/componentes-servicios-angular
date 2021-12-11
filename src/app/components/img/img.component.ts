b7f3f3fc4f411144b72c434811afedfcbd1d8d57
Antonio DroCode <antuansabe@gmail.com>
2021-12-10 21:49:25 -0600

    add lifecycle

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,20 +1,44 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit, OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent implements OnInit {
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
 
   @Input() img: string = '';
   @Output() loaded = new EventEmitter<string>();
   imageDefault = './assets/default.png';
 
-  constructor() { }
-  console.log("constructor", "imgValue =>", this.img);
-  ngOnInit(): void {
+  constructor() {
+    // before render
+    // No async -- once time
+    console.log("constructor", "imgValue =>", this.img);
   }
+
+ngOnChanges() {
+  // BEFORE -during  RENDER
+  // CHANGE INPUTS
+  console.log("ngOnchanges", "imgValue =>", this.img);
+}
+
+  ngOnInit(): void {
+    //before render
+    //async  - podemos hacer fetch y esas cosas -- once time
+    console.log("ngOnInit", "imgValue =>", this.img);
+  }
+
+ngAfterViewInit() {
+  //after render
+  //handler children
+  console.log('ngAfterViewInit');
+}
+
+ngOnDestroy() {
+  //delete
+  console.log("ngOnDestroy");
+}
 
   imgError() {
   this.img = this.imageDefault;
@@ -26,3 +50,18 @@ export class ImgComponent implements OnInit {
   }
 
 }
+
+/*
+Ciclo :
+
+Constructor: cuando se corre una instancia
+
+ngOnChanges : corre antes y durante en el render, siemrpe que detecte cambios en el Input, está para eso, para detectar los cambios.
+
+ngOnInit: corre antes pero tiene la condicione que solo correo una vez. Ahi se corren eventos asincronos.
+
+ngAfcterViewInit: corre cuando los hijos de ese componentes se han renderizado.
+
+NgOnDestroy: Corre cuando se elimina el componente.
+
+*/
