c32dc94c4f742a0acb3af16428bbe9398ad66f57
Antonio DroCode <antuansabe@gmail.com>
2022-02-07 12:47:04 -0600

    create structure of cms module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -64,6 +64,10 @@ const routes: Routes = [
       },
     ]
   },
+  {
+    path: 'cms',
+    loadChildren: ()=> import('./cms/cms.module').then ( m => m.CmsModule)
+  },
 
 {
   path: '**',
--- src/app/cms/cms-routing.module.ts
+++ src/app/cms/cms-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { TasksComponent } from './pages/tasks/tasks.component';
+import { GridComponent } from './pages/grid/grid.component';
+import { LayoutComponent } from './components/layout/layout.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'grid',
+        pathMatch: 'full'
+      },
+      {
+        path: 'task',
+        component: TasksComponent
+      },
+      {
+        path: 'grid',
+        component: GridComponent
+      },
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
